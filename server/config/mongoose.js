a9f3d67362ace7c05b83b9dfe9e5d91d8068cfe1
benmier <bencmier@gmail.com>
2016-04-22 23:58:32 -0700

    Update mongoose.js

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds041144.mlab.com:41144/heroku_fk6rj50x');
+mongoose.connect('mongodb://benmier:bluebird1@ds041144.mlab.com:41144/heroku_fk6rj50x');
 var models_path = __dirname + '/../models'
 
 fs.readdirSync(models_path).forEach(function(file) {
